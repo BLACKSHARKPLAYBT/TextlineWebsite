2209fc09cc3c6d5e67ed1d271d2e42b7f1b2c2cd
BLACKSHARKPLAYBT <blscksharkplaybt@gmail.com>
2024-12-28 18:33:21 +0800

    Site updated: 2024-12-28 18:33:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/28/LG G8 Thinq 9008 经历/","2024/12/24/期末元宇宙作业/"];function toRandomPost(){
+var posts=["2024/11/28/LG G8 Thinq 9008 经历/","2024/12/28/数学复习/","2024/12/24/期末元宇宙作业/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
